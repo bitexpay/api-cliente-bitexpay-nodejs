e2dd5b6ff54bf33cf3c3cd598c5e4b3b58505e36
sfloreano <sfloreano@vps593.com>
2022-05-17 14:08:30 -0500

    Test con node-fetch

--- api.js
+++ api.js
@@ -3,26 +3,21 @@ const qs = require('qs');
 const Config = require('./config');
 const Signature = require('./signature');
 
+import fetch from 'node-fetch';
+
 module.exports = function(app) {
     app.get('/api/test', (req, res) => {
-        
-        Request({
-            url: Config.URL + '/api/test',
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            json: true,
-        }, function (error, response, body) {
-            console.log(body);
-            res.send(body);
-        });
+        const url =  Config.URL + '/api/test';
+        console.log(url);
+
+        fetch(url)
+        .then(res => res.text())
+        .then(text => console.log(text));
     });
 
     app.get('/api/signature', (req, res) => {
         const body = Signature(req.query);
-        console.log(body);
-        // console.log( Config.URL + '/api/test/signature?' + qs.stringify(body));
+        const url =  Config.URL + '/api/test/signature?' + qs.stringify(body)
 
         Request({
             url: Config.URL + '/api/test/signature?' + qs.stringify(body),
