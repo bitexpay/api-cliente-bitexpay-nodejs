8e734a3aba3d91ab48f0f28c4f0c74a3980d7b7a
sfloreano <sfloreano@vps593.com>
2022-07-23 12:19:28 -0500

    Final

--- config.js
+++ config.js
@@ -1,10 +1,5 @@
 module.exports = {
     URL: "http://63.141.232.164:8001",
-<<<<<<< HEAD
     ACCESSKEY: "1Vp9VeQdvQu4hgukvlE0JPquTDl0Uj3S",
     SECRETKEY: "ZVRzVUrgMesgaOFO3UUXLEw6jkpHC1kfxmzMwnp6nkiogmKWW2QCY94tg5j9a6YA"
-=======
-    ACCESSKEY: "3W6E55m8U29wY52JJxyuPPp0b32UF6dIC",
-    SECRETKEY: "qV7Yn2pRkBHpBrPBz8d0t78SblqjnmFs8yfVAmK07Tz5QQBal8rBuCVYAomC900Q"
->>>>>>> other
 }
